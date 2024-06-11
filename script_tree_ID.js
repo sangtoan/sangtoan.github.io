8f5bb0d3b9b8b2f3ae3ea27ebda8f7f564b6f6e7
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-11 21:57:48 +0700

    Update script_tree_ID.js

--- script_tree_ID.js
+++ script_tree_ID.js
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     fetchedFiles.push({ path: file, content: content });
                     if (fetchedFiles.length === files.length) {
                         const treeData = buildTree(fetchedFiles);
-                        console.log('Tree structure:', treeData);  // Ghi lại cấu trúc cây
                         buildTreeView(treeContainer, treeData);
                     }
                 }).catch(error => {
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         files.forEach(file => {
             const exPattern = /\\begin{ex}.*?\\end{ex}/gs;
-            const idPattern = /\[(\d[A-Z]\d[YBKGTNHVC]\d\d)\]/;
+            const idPattern = /\[(\d[A-Z]\d[YBKGTNHVC]\d-\d)]/;
             let match;
             while ((match = exPattern.exec(file.content)) !== null) {
                 const element = match[0];
@@ -65,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 data[key].forEach(file => {
                     const fileLi = document.createElement('li');
                     fileLi.className = 'file';
-                    fileLi.textContent = parentKey + file.id.replace(/-/g, '');
+                    fileLi.textContent = parentKey + file.id + ' (' + file.path + ')';
                     fileLi.addEventListener('click', () => {
                         displayFileContent(file);
                     });
@@ -78,10 +77,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 dirLi.addEventListener('click', function (event) {
                     event.stopPropagation();
                     this.classList.toggle('expanded');
-                    const childUl = this.querySelector('ul');
-                    if (childUl) {
-                        childUl.classList.toggle('hidden');
-                    }
                 });
 
                 const childUl = document.createElement('ul');
@@ -95,17 +90,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function displayFileContent(file) {
         const resultContainer = document.getElementById('result-container');
-        const fileContentDiv = document.createElement('div');
-        fileContentDiv.className = 'file-content';
-        fileContentDiv.innerHTML = `
+        resultContainer.innerHTML = `
             <h2>${file.id}</h2>
             <pre>${file.content}</pre>
-            <button class="close-btn">X</button>
         `;
-        fileContentDiv.querySelector('.close-btn').addEventListener('click', () => {
-            resultContainer.removeChild(fileContentDiv);
-        });
-        resultContainer.appendChild(fileContentDiv);
     }
 
     function fetchFileContent(path) {
