1d2b4200ffe2ca883bd22694ca253457ff53a5df
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-13 16:25:48 +0700

    Update word2tex.js

--- word2tex.js
+++ word2tex.js
@@ -5,8 +5,13 @@ export function word2tex() {
     let inputCode = document.getElementById('inputCode').value;
 
     // Chuyển đổi cấu trúc câu hỏi và đáp án
-    const questionPattern = /Câu (\d+):([\s\S]*?)(?:\nA\.\s*(.*?)\nB\.\s*(.*?)\nC\.\s*(.*?)\nD\.\s*(.*?))(?:\nLời giải([\s\S]*?))?(?=\nCâu \d|$)/g;
+    const questionPattern = /Câu (\d+):([\s\S]*?)(?:\nA\.\s*(.*?)\nB\.\s*(.*?)\nC\.\s*(.*?)\nD\.\s*(.*?))?(?:\nLời giải([\s\S]*?))?(?=\nCâu \d|$)/g;
     inputCode = inputCode.replace(questionPattern, (match, num, questionContent, choiceA, choiceB, choiceC, choiceD, solution) => {
+        // Kiểm tra nếu không đủ 4 phương án
+        if (!choiceA || !choiceB || !choiceC || !choiceD) {
+            return `<span style="background-color: yellow;">${match}</span>`;
+        }
+
         let result = `%% Câu ${num}:\n\\begin{ex}\n${questionContent.trim()}\n\\choice\n{${choiceA.trim()}}\n{${choiceB.trim()}}\n{${choiceC.trim()}}\n{${choiceD.trim()}}\n\\loigiai{\n${solution ? solution.trim() : ''}\n}\n\\end{ex}\n`;
         return result;
     });
