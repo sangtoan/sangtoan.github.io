84317187f5f590b220b7b2bcf43f2a805a03da9b
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-13 16:13:43 +0700

    Update numberUtils.js

--- numberUtils.js
+++ numberUtils.js
@@ -1,5 +1,6 @@
 export function convertNumberToMathMode(text) {
-    const pattern = /(\b\w+\b\s)(\d+)(\s\b\w+\b)/g;
+    // Mẫu regex nhận dạng số nguyên và số thập phân giữa các từ
+    const pattern = /(\b\w+\b\s)(\d+(\.\d+)?)(\s\b\w+\b)/g;
     return text.replace(pattern, (match, before, number, after) => {
         if (/[a-zA-Z]/.test(before) && /[a-zA-Z]/.test(after)) {
             return `${before} $${number}$ ${after}`;
