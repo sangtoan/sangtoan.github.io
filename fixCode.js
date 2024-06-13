ff5ddcfcad34367b1a468dfadb3fc5b9a6c84c46
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-13 15:53:01 +0700

    Create fixCode.js

---
+++ fixCode.js
@@ -0,0 +1,17 @@
+import { convertNumberToMathMode } from './numberUtils.js';
+import { replaceTextWithJson } from './replaceUtils.js';
+
+export function fixCode() {
+    let inputCode = document.getElementById('inputCode').value;
+    inputCode = inputCode.replace(/\\mathrm{R}/g, '\\mathbb{R}');
+    inputCode = convertNumberToMathMode(inputCode);
+    inputCode = inputCode.replace(/}\s*{/g, '}{');
+
+    fetch('replace.json')
+        .then(response => response.json())
+        .then(data => {
+            inputCode = replaceTextWithJson(data, inputCode);
+            document.getElementById('outputCode').value = inputCode;
+        })
+        .catch(error => console.error('Error:', error));
+}
