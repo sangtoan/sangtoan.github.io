3152124011071fe7328723559d1ee71ef8e5ddee
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-13 15:53:55 +0700

    Create word2tex.js

---
+++ word2tex.js
@@ -0,0 +1,26 @@
+import { convertNumberToMathMode } from './numberUtils.js';
+import { replaceTextWithJson } from './replaceUtils.js';
+
+export function word2tex() {
+    let inputCode = document.getElementById('inputCode').value;
+
+    // Chuyển đổi cấu trúc câu hỏi và đáp án
+    const questionPattern = /Câu (\d+):([\s\S]*?)\nA\.\s*(.*?)\nB\.\s*(.*?)\nC\.\s*(.*?)\nD\.\s*(.*?)(\nLời giải([\s\S]*?))?\n/g;
+    inputCode = inputCode.replace(questionPattern, (match, num, questionContent, choiceA, choiceB, choiceC, choiceD, solutionMatch, solution) => {
+        let result = `%% Câu ${num}:\n\\begin{ex}\n${questionContent.trim()}\n\\choice\n{${choiceA.trim()}}\n{${choiceB.trim()}}\n{${choiceC.trim()}}\n{${choiceD.trim()}}\n\\loigiai{\n${solution ? solution.trim() : ''}\n}\n\\end{ex}\n`;
+        return result;
+    });
+
+    // Thay thế ký hiệu toán học
+    inputCode = inputCode.replace(/\\mathrm{R}/g, '\\mathbb{R}');
+    inputCode = convertNumberToMathMode(inputCode);
+    inputCode = inputCode.replace(/}\s*{/g, '}{');
+
+    fetch('replace.json')
+        .then(response => response.json())
+        .then(data => {
+            inputCode = replaceTextWithJson(data, inputCode);
+            document.getElementById('outputCode').value = inputCode;
+        })
+        .catch(error => console.error('Error:', error));
+}
