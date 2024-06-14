a8e4a14ca781301ee0905cd3f4400d04d78a8eea
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-14 11:48:22 +0700

    Update numberUtils.js

--- numberUtils.js
+++ numberUtils.js
@@ -2,6 +2,8 @@ export function convertNumberToMathMode(text) {
     // Mẫu regex nhận dạng số nguyên và số thập phân giữa các từ
     const inlinePattern = /(\b\w+\b\s)(\d+(\.\d+)?)(\s\b\w+\b)/g;
     const linePattern = /^\s*\{\s*(\d+(\.\d+)?)\s*\.?\s*\}\s*$/gm;
+    const mathrmPattern = /\\mathrm{([^dACP\s][a-zA-Z]*)}/g;
+    const mathrmTildePattern = /\\mathrm{\~([^dACP\s][a-zA-Z]*)}/g;
 
     // Thay thế các số nguyên và số thập phân giữa các từ
     text = text.replace(inlinePattern, (match, before, number, after) => {
@@ -14,6 +16,16 @@ export function convertNumberToMathMode(text) {
     // Thay thế các dòng chỉ có dạng { \d .} hoặc { \d }
     text = text.replace(linePattern, (match, number) => {
         return `{$${number}$}`;
+    });
+
+    // Thay thế \mathrm{[a-zA-Z]} thành [a-zA-Z], ngoại trừ d, A, C, P
+    text = text.replace(mathrmPattern, (match, char) => {
+        return `${char}`;
+    });
+
+    // Thay thế \mathrm{~[a-zA-Z]} thành [a-zA-Z], ngoại trừ d, A, C, P
+    text = text.replace(mathrmTildePattern, (match, char) => {
+        return `${char}`;
     });
 
     return text;
